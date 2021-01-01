114f2cce100028e8a6c044888013b4de3f6a76f4
nimit patel <nimit.patel@sjsu.edu>
2020-12-31 18:06:29 -0800

    start

---
+++ RomanNumeral_1.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+#include "RomanNumeral.h"
+
+using namespace std;
+
+RomanNumeral::RomanNumeral() : roman(""), decimal(0) {}
+
+/***** Complete this class implementation. *****/
---
+++ RomanNumeral_1.h
@@ -0,0 +1,18 @@
+#ifndef ROMANNUMERAL_H_
+#define ROMANNUMERAL_H_
+
+#include <string>
+using namespace std;
+
+class RomanNumeral
+{
+public:
+    /**
+     * Default constructor.
+     */
+    RomanNumeral();
+
+    /***** Complete this class declaration. *****/
+};
+
+#endif /* ROMANNUMERAL_H_ */
