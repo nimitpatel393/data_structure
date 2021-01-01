6d5e7d9505329536bdb99c7ff98b86736042a519
nimit patel <nimit.patel@sjsu.edu>
2020-12-31 18:02:15 -0800

    final

--- students_1.cpp
+++ students_1.cpp
@@ -1,216 +1,142 @@
-/**
- * Assignment 4. Student scores
- * Spring 2020
- *
- * Build dynamic arrays to store student names and scores
- * read from an input file. Access the arrays to print the
- * student names and scores.
- *
- * Author: Ron Mak
- *         Department of Computer Engineering
- *         San Jose State University
- */
 #include <iostream>
 #include <fstream>
 #include <iomanip>
 #include <string>
+#include <sstream>
 
 using namespace std;
 
-const string INPUT_FILE_NAME = "students.txt";
+void delete_arrays(string *names, int **scores, int count);
+void extractor(string each_line, string *names, int **scores, int count);
+void score_dump(string numbers, int **scores, int count);
+void print(string *names, int **scores, int count);
 
-typedef string   *NamePtr;   // -> a student name
-typedef string   *NameArray; // -> dynamic array of student names
-typedef int      *ScorePtr;  // -> a score
-typedef int      *ScoreRow;  // -> dynamic array (row) of student scores
-typedef ScoreRow *RowArray;  // -> dynamic array of pointers to score rows
-typedef ScoreRow *RowPtr;    // -> a pointer to a score row
+const string student_file_name = "students.txt";
+uint8_t extract_student_number(string first_line);
 
-/**
- * Read the course name and the count of students.
- * Create dynamic arrays for the student names and scores.
- * @param input the input file stream.
- * @param course the course name.
- * @param names the dynamic array of names.
- * @param rows the dynamic array of pointers to the rows of scores.
- * @return the count of students.
- */
-int create_dynamic_arrays(ifstream& input, string& course,
-                          NameArray& names, RowArray& rows);
+bool first_line_flag = 1;
+int count;
 
-/**
- * Fill the dynamic array of student names and the
- * dynamic array of pointers to the score rows.
- * @param input the input stream.
- * @param student_count the count of students.
- * @param names the dynamic array of student names.
- * @param names the dynamic array of pointers to the score rows.
- */
-void fill_dynamic_arrays(ifstream& input, int student_count,
-                         const NameArray names, const RowArray rows);
-
-/**
- * Read a student's assignment scores and fill
- * the dynamic integer array (row) of the scores.
- * @param input the input stream.
- * @param score_count the count of scores for this student.
- * @param row_ptr the pointer to the score row.
- */
-void fill_score_row(ifstream& input, int score_count, const RowPtr row_ptr);
-
-/**
- * Print the scores for each student in the course.
- * @param course the name of the course.
- * @param names the dynamic array of student names.
- * @param rows the dynamic array of pointers to the scores rows.
- */
-void print_student_scores(const string course, int student_count,
-                          const NameArray names, const RowArray rows);
-
-/**
- * Deallocate the dynamic arrays.
- * @param student_count the count of students.
- * @param names the dynamic array of student names.
- * @param rows the dynamic array of pointers to the scores rows.
- */
-void delete_dynamic_arrays(int student_count, NameArray names, RowArray rows);
-
-/**
- * Main.
- */
 int main()
 {
-    ifstream input;
-    input.open(INPUT_FILE_NAME);
-    if (input.fail())
+    ifstream student_input_stream;
+    student_input_stream.open(student_file_name);
+    if (student_input_stream.fail())
     {
-        cout << "Failed to open " << INPUT_FILE_NAME << endl;
+        cout << "Failed to open " << student_file_name << endl;
         return -1;
     }
 
-    string course;
-    NameArray names = nullptr;  // -> array of student names
-    RowArray  rows  = nullptr;  // -> array of pointers to the
-                                //    rows of student scores
+    /***** Complete this program. *****/
+    string temp;
 
-    int student_count = create_dynamic_arrays(input, course, names, rows);
-    print_student_scores(course, student_count, names, rows);
-    delete_dynamic_arrays(student_count, names, rows);
+    getline(student_input_stream, temp);
 
+    uint8_t student_count = extract_student_number(temp);
+
+    string *names = new string[student_count];
+    int **scores = new int *[student_count];
+
+    //cout << *names << *scores << endl;
+
+    while (getline(student_input_stream, temp))
+    {
+        extractor(temp, names, scores, count);
+        count++;
+    }
+    print(names, scores, student_count);
+    delete_arrays(names, scores, student_count);
     return 0;
 }
 
-int create_dynamic_arrays(ifstream& input, string& course,
-                          NameArray& names, RowArray& rows)
+uint8_t extract_student_number(string first_line)
 {
-    int student_count;
+    string student_num = first_line.substr(4, string::npos);
+    //cout << student_num << endl;
+    uint8_t temp_student = stoi(student_num, nullptr, 10);
+    first_line_flag = 0;
 
-    // Read the course name and student count.
-    // Create the dynamic array of student names and the
-    // dynamic array of pointers to the rows of student scores.
-    input >> course >> student_count;
-    names = new string[student_count];
-    rows  = new ScoreRow[student_count];
-
-    // Create the dynamic array of pointers
-    // to the rows of student scores.
-    fill_dynamic_arrays(input, student_count, names, rows);
-
-    return student_count;
+    return temp_student;
 }
 
-void fill_dynamic_arrays(ifstream& input, int student_count,
-                         const NameArray names, const RowArray rows)
+void delete_arrays(string *names, int **scores, int count)
 {
-    int score_count;
-    string first_name, last_name;
-    NamePtr name_ptr = names;  // -> first name of the names array
-    RowPtr  row_ptr  = rows;   // -> first pointer to the score rows
-
-    // Loop once per student.
-    while (student_count > 0)
+    for (int i = 0; i < count; i++)
     {
-        // Read a student name and the number of assignments.
-        // Use name_ptr to enter the name into the name array.
-        input >> first_name >> last_name >> score_count;
-        *name_ptr = first_name + " " + last_name;
-
-        // Create a dynamic integer array as a row of student scores.
-        // Make it one larger for the -1 sentinel at the end. Fill the array.
-        *row_ptr = new int[score_count + 1];
-        fill_score_row(input, score_count, row_ptr);
-
-        name_ptr++;       // -> next element of the name array
-        row_ptr++;        // -> next element of the row array
-        student_count--;  // decrement the copy of the student count
+        //cout << names[i] << scores[i][1] << endl;
+        delete[] scores[i];
     }
+    delete[] names;
+    delete[] scores;
 }
 
-void fill_score_row(ifstream& input, int score_count, const RowPtr row_ptr)
+void extractor(string each_line, string *names, int **scores, int count)
 {
-    ScorePtr score_ptr = *row_ptr;  // -> first score of the row
+    string space = " ", name, numbers;
+    int pos = each_line.find(space), counter = 0;
+    //cout<<"Here"<<pos<<endl;
 
-    // Loop once per assignment score of the student.
-    while (score_count > 0)
+    do
     {
-        input >> *score_ptr;  // read the next score into the score row
-        score_ptr++;          // -> next score of the row
-        score_count--;        // decrement the copy of the score count
-    }
-
-    *score_ptr = -1;  // sentinel to mark the end of this score row
-}
-
-void print_student_scores(const string course, int student_count,
-                          const NameArray names, const RowArray rows)
-{
-    NamePtr name_ptr = names;  // -> first name of student names
-    RowPtr  row_ptr  = rows;   // -> first pointer to the score rows
-
-    cout << "STUDENT SCORES for " << course << endl;
-
-    // Loop once per row of student scores.
-    while (student_count > 0)
-    {
-        cout << endl;
-        cout << *name_ptr << endl;  // print the student's name
-
-        int *score_ptr = *row_ptr;  // -> first score of the row
-
-        // No scores?
-        if (*score_ptr < 0)
+        // cout << pos << endl;
+        pos = each_line.find(space, pos + 1);
+        counter++;
+        if (counter == 1)
         {
-            cout << "   (none)";
+            name = each_line.substr(0, pos);
+            numbers = each_line.substr(pos + 1);
+            names[count] = name;
+            //cout<<name<<endl<<numbers<<endl;
         }
+    } while (pos != string::npos);
+    score_dump(numbers, scores, count);
+    //counter=0;
+}
 
-        // Else loop to print each score in the row.
-        // Stop at the -1 sentinel.
-        else while (*score_ptr >= 0)
+void score_dump(string numbers, int **scores, int count)
+{
+    //cout<<numbers<<endl;
+    string space = " ", temp;
+    int iteration, o_pos, size, pos = numbers.find(space);
+
+    temp = numbers.substr(0, pos);
+    iteration = stoi(temp, nullptr, 10);
+    scores[count] = new int[iteration];
+    //  numbers = numbers.substr(2);
+    //pos = numbers.find(space);
+
+    //cout << iteration << endl;
+    for (int i = 0; i < iteration + 1; i++)
+    {
+        //cout << numbers << endl;
+        if (pos != string::npos)
         {
-            cout << setw(5) << *score_ptr;
-            score_ptr++;  // -> next score of the row
+            o_pos = pos;
+            pos = numbers.find(space, pos + 1);
+            temp = numbers.substr(o_pos, pos - o_pos);
+            //cout << temp << endl;
+            scores[count][i] = stoi(temp);
+            // cout << scores[count][i] << "index " << count << i << endl;
         }
-        cout << endl;
-
-        name_ptr++;       // -> next name in the name array
-        row_ptr++;        // -> next pointer to a score row
-        student_count--;  // decrement the copy of the student count
     }
 }
 
-void delete_dynamic_arrays(int student_count, NameArray names, RowArray rows)
+void print(string *names, int **scores, int count)
 {
-    RowPtr row_ptr = rows;  // -> the first row pointer
-
-    // Loop once per row of student scores.
-    while (student_count > 0)
+    cout << "STUDENT SCORES for C++" << endl
+         << endl;
+    for (int i = 0; i < count; i++)
     {
-        delete [] *row_ptr;  // delete the row of scores
-
-        row_ptr++;           // -> next pointer to a score row
-        student_count--;     // decrement the copy of the student count
+        int ii = 0;
+        cout << names[i] << endl;
+        if (scores[i][ii] == 0)
+            cout << "(none)";
+        while (scores[i][ii] != 0)
+        {
+            cout << scores[i][ii] << " ";
+            ii++;
+        }
+        cout << endl
+             << endl;
     }
-
-    delete [] rows;   // delete the array of row pointers
-    delete [] names;  // delete the array of names
 }
